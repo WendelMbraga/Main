9b345ed0a1f17f946300ad7ac953d8f4d8fbc1ce
unknown <wendel984143238@gmail.com>
2021-12-13 16:39:37 -0400

    removendo modulos

---
+++ src/Pages/Frase/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Frase(){
+    return(
+        <h1>"Sogeking, senta o dedo naquela bandeira" -Luffy 
+            <p>Anime: One piece</p> </h1>
+    );
+};
+
+export default Frase;
